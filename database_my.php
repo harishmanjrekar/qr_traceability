400dda074ba4df3d135a52b2f4271f7e8a912f10
harishmanjrekar <54299326+harishmanjrekar@users.noreply.github.com>
2023-07-06 18:40:21 +0530

    Update database_my.php

--- database_my.php
+++ database_my.php
@@ -1,5 +1,5 @@
 <?php
-    $connectionInfo = array("UID" => "rih", "pwd" => "Adminpassword!", "Database" => "blockchain", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+    $connectionInfo = array("UID" => "rih", "pwd" => "Adminpassword!!", "Database" => "blockchain", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:rih1.database.windows.net,1433";
 $connection = sqlsrv_connect($serverName, $connectionInfo);
 ?>
