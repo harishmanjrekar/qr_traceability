d2dedbfb865903a0da42cbd242c8e843c778f7e0
harishmanjrekar <54299326+harishmanjrekar@users.noreply.github.com>
2023-07-06 15:41:21 +0530

    Update database_my.php

--- database_my.php
+++ database_my.php
@@ -1,16 +1,16 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $connection = new PDO("sqlsrv:server = tcp:rih1.database.windows.net,1433; Database = blockchain", "rih", "{your_password_here}");
+    $connection = new PDO("sqlsrv:server = tcp:rih1.database.windows.net,1433; Database = blockchain", "rih", "Adminpassword!");
     $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
+    print("Error connecting to SQL Server ABCDEFG.");
     die(print_r($e));
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "rih", "pwd" => "{your_password_here}", "Database" => "blockchain", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "rih", "pwd" => "Adminpassword", "Database" => "blockchain", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:rih1.database.windows.net,1433";
 $connection = sqlsrv_connect($serverName, $connectionInfo);
 ?>
