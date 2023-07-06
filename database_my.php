fdf2e5507c88e1fe89ca724f161f6b1eea3131b5
harishmanjrekar <54299326+harishmanjrekar@users.noreply.github.com>
2023-07-06 17:40:04 +0530

    Update database_my.php

--- database_my.php
+++ database_my.php
@@ -1,12 +1,12 @@
+
 <?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $connection = new PDO("sqlsrv:server = tcp:rih1.database.windows.net,1433; Database = blockchain", "rih", "Adminpassword!");
-    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server ABCDEFG.");
-    die(print_r($e));
-}
+    $serverName = "tcp:rih1.database.windows.net,1433"; // update me
+    $connectionOptions = array(
+        "Database" => "blockchain", // update me
+        "Uid" => "rih", // update me
+        "PWD" => "Adminpassword!" // update me
+    );
+    //Establishes the connection
+    $connection = sqlsrv_connect($serverName, $connectionOptions);
 
 ?>
